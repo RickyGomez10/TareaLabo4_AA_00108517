57f3a7b909a12a3dd036bc30f202a52f1a43336a
RickyGomez10 <00108517@uca.edu.sv>
2019-09-18 20:52:19 -0600

    Tarea labo 4 Analisis Numerico

---
+++ cmake-build-debug/CMakeFiles/MissingNumber.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MissingNumber.dir/main.cpp.obj"
+  "MissingNumber.pdb"
+  "MissingNumber.exe"
+  "MissingNumber.exe.manifest"
+  "libMissingNumber.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MissingNumber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
